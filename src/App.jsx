752d00d07c8bc1e09ba07b64fe7df66e0c150eb5
Anjali <aniupadhyay512@gmail.com>
2026-01-18 20:57:02 +0530

    portfolio update

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './section/Navbar';
+import Hero from './section/Hero';
+import About from './section/About';
+
+const App = () => {
+  return ( 
+  <div className="container mx-auto max-w-7xl">
+    <Navbar />
+    <Hero />
+    <About />
+    <section className="min-h-screen" />
+    <section className="min-h-screen" />
+    <section className="min-h-screen" />
+    <section className="min-h-screen" />
+    <section className="min-h-screen" />
+    {/* about */}
+    {/* experience */}
+    {/* work */}
+    {/* contact */}
+    {/* footer */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/section/About.jsx
@@ -0,0 +1,50 @@
+import { useRef } from "react";
+import Card from "../components/Card";
+
+const About = () => {
+    const grid2Container = useRef();
+    return (
+        <section className="c-space section-spacing">
+            <h2 className="text-heading">About Me</h2>
+            <div 
+            className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12">
+                {/*Grid Item 1*/}
+                <div className="flex items-end grid-default-color grid-1">
+                    <img
+                    src="/assets/coding-pov.png" 
+                    className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]" />
+                    <div className="z-10">
+                        <p className="headtext">Hi, I'm Anjali</p>
+                        <p className="subtext">Over the last 2+years I developed my backend and frontend skills to deliver scalable projects.</p>
+                    </div>
+                    <div className="absolute inset-x-0 pointer-events-none-bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo"/>
+                </div>
+                {/*Grid Item 2*/}
+                <div className="grid-default-color grid-2">
+                    <div ref={grid2Container} className="flex items-center justify-center w-full h-full">
+                        <p className="flex items-end text-5xl text-gray-500">
+                            CODE IS CRAFT
+                        </p>
+                        <Card style={{ rotate: "35deg", top:"40%", left: "30%"}} text="Designing" containerRef={grid2Container}/>
+                        <Card style={{ rotate: "-30deg", top:"60%", left: "60%"}} text="Building" containerRef={grid2Container}/>
+                        <Card style={{ rotate: "90deg", bottom:"30%", left: "10%"}} text="Grasp" containerRef={grid2Container}/>
+                        <Card style={{ rotate: "-75deg", top:"30%", left: "-7%"}} text="Team player" containerRef={grid2Container}/>
+                        <Card style={{ rotate: "20deg", top:"10%", left: "38%"}} text="Solid" containerRef={grid2Container}/>
+                        <Card style={{ rotate: "30deg", top: "70%", left: "70%" }}image="assets/logos/csharp-pink.png" containerRef={grid2Container}/>
+                        <Card style={{ rotate: "-45deg", top: "70%", left: "25%" }}image="assets/logos/dotnet-pink.png" containerRef={grid2Container}/>
+                        <Card style={{ rotate: "-45deg", top: "5%", left: "10%" }}image="assets/logos/blazor-pink.png"containerRef={grid2Container}/>
+                    </div>
+                </div>
+                {/*Grid Item 3*/}
+                <div className="grid-black-color grid-3"></div>
+                {/*Grid Item 4*/}
+                <div className="grid-special-color grid-4"></div>
+                {/*Grid Item 5*/}
+                <div className="grid-default-color grid-5"></div>
+
+            </div>
+        </section>
+    );
+};
+
+export default About;
