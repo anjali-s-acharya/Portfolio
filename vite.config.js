3459f429d075206ef8444fc355ee13dbe7c8851d
Anjali <aniupadhyay512@gmail.com>
2026-01-26 21:30:59 +0530

    fix: updated vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,17 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'three-vendor': ['three', '@react-three/fiber', '@react-three/drei']
+        }
+      }
+    }
+  },
+  resolve: {
+    dedupe: ['react', 'react-dom']
+  }
 })
